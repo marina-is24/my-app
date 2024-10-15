0bbd9e3e7a8a7d421caa9ff37f957b3eaa3b7bef
Marina <marina@egrowth-inc.com>
2024-10-15 16:41:38 +0900

    publish comment feature

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'blogproject/my-app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8c2cf0703f8490ce8ed2dddf891139aff626ea11',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'blogproject/my-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8c2cf0703f8490ce8ed2dddf891139aff626ea11',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
